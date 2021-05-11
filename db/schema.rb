ae4afe7e3b6f488fb148900f21d1125f3904c85b
Fares <ashraffares090@gmail.com>
2021-05-11 12:11:54 +0200

    Add username to users model user can sign in with name or email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_11_090541) do
+ActiveRecord::Schema.define(version: 2021_05_11_090903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,8 +23,10 @@ ActiveRecord::Schema.define(version: 2021_05_11_090541) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "user"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["user"], name: "index_users_on_user", unique: true
   end
 
 end
