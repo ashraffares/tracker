7c6c4cb5ecfae80c56415b3e716c1dcfa8348dc4
Fares <ashraffares090@gmail.com>
2021-05-11 23:44:35 +0200

    Add :user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # app/models/user.rb
 
+  validates :user, presence: true, uniqueness: { case_sensitive: false }
   has_one_attached :avatar,dependent: :destroy
   validate :validate_user
   attr_writer :login
@@ -15,7 +17,7 @@ class User < ApplicationRecord
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
-      where(conditions.to_h).where(["lower(user) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      where(conditions.to_h).where(['lower(user) = :value OR lower(email) = :value', { :value => login.downcase }]).first
     elsif conditions.has_key?(:user) || conditions.has_key?(:email)
       where(conditions.to_h).first
     end
