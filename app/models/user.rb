5d1a39a5e54afce2f9ee5ae16ded8671325b02a1
Fares <ashraffares090@gmail.com>
2021-05-12 13:55:36 +0200

    Fix nameing error

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,27 +5,25 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   # app/models/user.rb
 
-  validates :user, presence: true, uniqueness: { case_sensitive: false }
-  has_one_attached :avatar,dependent: :destroy
-  validate :validate_user
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  has_one_attached :avatar, dependent: :destroy
+  validate :validate_username
   attr_writer :login
 
   def login
-    @login || self.user || self.email
+    @login || username || email
   end
 
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
-      where(conditions.to_h).where(['lower(user) = :value OR lower(email) = :value', { :value => login.downcase }]).first
-    elsif conditions.has_key?(:user) || conditions.has_key?(:email)
+      where(conditions.to_h).where(['lower(username) = :value OR lower(email) = :value', { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
       where(conditions.to_h).first
     end
   end
 
-  def validate_user
-    if User.where(email: user).exists?
-      errors.add(:user, :invalid)
-    end
+  def validate_username
+    errors.add(:username, :invalid) if User.where(email: username).exists?
   end
 end
