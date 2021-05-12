6bedb4070dd5e142dbab883587086eac13d21748
Fares <ashraffares090@gmail.com>
2021-05-12 11:25:17 +0200

    Fix conflicting error name user with admin user to username

--- db/schema.rb
+++ db/schema.rb
@@ -51,10 +51,10 @@ ActiveRecord::Schema.define(version: 2021_05_11_132705) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "user"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["user"], name: "index_users_on_user", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
