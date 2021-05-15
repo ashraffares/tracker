0ffaa46050e6b291762a12370a8192527b59b280
Fares <ashraffares090@gmail.com>
2021-05-15 14:55:48 +0200

    Add chart to index page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,6 @@
 class UserController < ApplicationController
   before_action :authenticate_user!
   def index
-    @user = User.where(id: current_user.id).includes(:items, :groups).all
+    @user = User.includes(:items, :groups).all
   end
 end
