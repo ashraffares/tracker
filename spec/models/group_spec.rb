9ba2c8a7891731b87abd5e233b865efcf867228c
Fares <ashraffares090@gmail.com>
2021-05-16 18:50:04 +0200

    Add more test for models

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,6 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
+  describe 'new group' do
+    user = User.new(id: 1, username: 'fares', email: 'fares@gamil.com', password: '000000')
+    group1 = user.groups.build(id: 1, name: 'mygroup')
+    group1_items = group1.items.build(id: 1, name: 'myitem', amount: 100, user_id: 1, group_id: 1)
+
+    it 'should display group name' do
+      expect(group1.name).to eq('mygroup')
+    end
+    it 'should display group author' do
+      expect(group1.user.username).to eq('fares')
+    end
+    it 'should display group author' do
+      expect(group1.user.username).to eq('fares')
+    end
+    it 'should display group items name' do
+      expect(group1_items.name).to eq('myitem')
+    end
+    it 'should display group items amount' do
+      expect(group1_items.amount).to eq(100.0)
+    end
+  end
+
   describe 'association and vaildation' do
     it 'should has_many items' do
       expect { should has_many(:items) }
