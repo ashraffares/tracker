d296ea48a5a51669e2eb2fd8e77d25f6b32ad48c
Fares <ashraffares090@gmail.com>
2021-05-17 16:44:16 +0200

    re deploy the app

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,5 +1,3 @@
-git remote add origin <URL to new heroku app># Be sure to restart your server when you modify this file.
-
 # Configure sensitive parameters which will be filtered from the log file.
 Rails.application.config.filter_parameters += [
   :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn
