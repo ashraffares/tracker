089cdd88cc7fa4023c1463f1834b2ceb25d8f564
Fares <ashraffares090@gmail.com>
2021-05-17 20:31:42 +0200

    Add max value for items

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,5 @@ class Item < ApplicationRecord
   belongs_to :group, optional: true
 
   validates :name, presence: true
-  validates :amount, presence: true, numericality: { greater_than: 0 }
+  validates :amount, presence: true, numericality: { greater_than: 0, less_than_or_equal_to: 1000.0}
 end
